d4a004ad1515bb421efbb0b3e3f19283169bf114
Moo-YewTsing <mooyewtsing@gmail.com>
2018-12-07 18:49:28 +0800

    gpu

--- Unet_trainor.py
+++ Unet_trainor.py
@@ -6,18 +6,18 @@ import sys
 
 def model_train(X_train, Y_train, X_test, Y_test, model_name=None, model_p=None):
     if not model_name:
-        model_name = 'model-eos3.h5' 
+        model_name = 'model-eos3' 
     model = UNET()
     if model_p:
         model.load_weights(model_p)
 
     earlystopper = EarlyStopping(patience=5, verbose=1)
-    checkpointer = ModelCheckpoint(model_name, verbose=1, save_best_only=True)
-    model.fit(X_train, Y_train, batch_size=32,
+    checkpointer = ModelCheckpoint(model_name+'.h5', verbose=1, save_best_only=True)
+    model.fit(X_train, Y_train, batch_size=16,
             verbose=1, epochs=100,
             validation_data=(X_test, Y_test),
             callbacks = [earlystopper, checkpointer,
-                        TensorBoard(log_dir='cell_log', histogram_freq=0)])
+                        TensorBoard(log_dir=model_name+'_log', histogram_freq=0)])
     return model
 
 def training_from_dir(dir_img, dir_label, model_name=None, model_p=None):
