aeb0dfdddd7637bff6c6724874c5f58d7e3488cd
Moo-YewTsing <37372924+Moo-YewTsing@users.noreply.github.com>
2018-12-08 12:44:15 +0800

    add

---
+++ gif_maker.py
@@ -0,0 +1,53 @@
+from PIL import Image, ImageDraw, ImageFont
+import os
+import sys
+from collections import defaultdict
+from EOS_tools import get_name, path_list_creator
+from os.path import join
+
+def make_gif_from_dir(metric_dir_path, dst):
+    maid = defaultdict(dict)
+    path_list = path_list_creator(metric_dir_path)
+    for fp in path_list:
+        name = get_name(fp)
+        if '_pred' in fp:
+            maid[name]['predicted'] = fp
+        elif '_label' in fp:
+            maid[name]['labeled'] = fp
+        # elif '_fit' in fp:
+        #     maid[name]['fit'] = fp
+    for f_name, fp_dict in maid.items():
+        if len(fp_dict) == 2:
+            out_p = join(dst, f_name+'.gif')
+            make_gif(fp_dict, out_p)
+
+def img_makeup(img, text, f_size=100):
+    fnt = ImageFont.truetype('Aaargh.ttf', f_size)
+    draw = ImageDraw.Draw(img)
+    text_w, text_h = draw.textsize(text, fnt)
+    w, h = img.size
+    x = int((w - text_w) / 2)
+    y = int((h- text_h) / 2)
+    draw.text((x, y), text, font=fnt)
+    return img
+
+def make_gif(fp_dict, out_p):
+    images = []
+    for element in ('predicted', 'labeled'):
+        fp = fp_dict[element]
+        img = Image.open(fp)
+        img_up = img_makeup(img, element.upper())
+        images.append(img_up)
+        print(fp)
+
+    images[0].save(out_p,
+               save_all=True,
+               append_images=images[1:],
+               duration=666,
+               loop=0)
+
+if __name__ == "__main__":
+    start = sys.argv[1]
+    dst = sys.argv[2]
+    os.makedirs(dst, exist_ok=True)
+    make_gif_from_dir(start, dst)
