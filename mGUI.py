b165bb3379bb04d7ee3cfffe85f5c6f2e8698cf9
Moo-YewTsing <37372924+Moo-YewTsing@users.noreply.github.com>
2018-12-15 11:26:59 +0800

    still bug

--- mGUI.py
+++ mGUI.py
@@ -13,6 +13,7 @@ import numpy as np
 import compileall
 from functools import partial
 from Unet_box.use_model import Unet_predictor
+from collections import deque
 
 def ask_path():
     global dst
@@ -35,7 +36,7 @@ class PictureWindow(Canvas):
         self.type_to_suffix = {'detection':'_pred', 'mask':'_mask'}
         self.path_list = creat_path_list()
         self.cur = 0
-        self.cur_type = ['origin']
+        self.cur_type = deque(maxlen=2)
 
         self.cache = {}
         self.length = len(self.path_list)
@@ -84,36 +85,40 @@ class PictureWindow(Canvas):
         self._button_disable()
         if update_predicted == True:
             print('prediction completed')
-            self._show_something(cache, 'detection', next_img_type='origin')
+            self._img_and_button(cache, 'detection', next_img_type='origin')
         else:
             if isinstance(cache[1], np.ndarray):
-                self._show_something(cache, 'origin', next_img_type='detection')
+                self._img_and_button(cache, 'origin', next_img_type='detection')
             else:
-                self._show_something(cache, 'origin')
+                self._img_and_button(cache, 'origin')
                 return
             if isinstance(cache[2], np.ndarray):
-                mask_type = 'mask'
-                self._button_enable(f"show {mask_type}", 
-                partial(self._show_something, cache, mask_type, next_img_type=self.cur_type), 
-                button_type=mask_type)
+                self._button_update(cache, 'mask', 'mask')
 
-    def _show_something(self, cache, img_type:str, next_img_type=None, dis_button=None):
+    def _button_update(self, cache, img_type, button_type):
+        self._button_disable(button_type=button_type)
+        self._button_enable(f"show {img_type}", 
+        partial(self._img_and_button, cache, img_type, next_img_type=self.cur_type[0]), 
+        button_type=button_type)
+
+    def _show_something(self, cache, img_type:str):
         """self.type_to_idx = {'origin':0, 'detection':1, 'mask':2}"""
-        self.cur_type = img_type
+        self.cur_type.append(img_type)
         idx = self.type_to_idx[img_type]
         print(f'show {img_type}')
         self.show_image(cache[idx])
+
+    def _img_and_button(self, cache, img_type:str, next_img_type=None, dis_button=None):
+        self._show_something(cache, img_type)
+        self._button_handel(cache, next_img_type, dis_button)
+
+    def _button_handel(self, cache, next_img_type=None, dis_button=None):
         if dis_button:
             self._button_disable(button_type=dis_button)
-        else:
-            self._button_disable(button_type=img_type)
-
         if next_img_type:
-            button_type = img_type if img_type == 'mask' else next_img_type
-            self._button_enable(f"show {next_img_type}", 
-            partial(self._show_something, cache, next_img_type, next_img_type=self.cur_type, 
-            dis_button=button_type), 
-            button_type=button_type)
+            print(self.cur_type)
+            button_type = 'mask' if self.cur_type[1] == 'mask' else 'origin'
+            self._button_update(cache, next_img_type, button_type)
 
     def _button_disable(self, button_type=None):
         iters = self.type_to_button.keys() if not button_type else [button_type]
@@ -124,7 +129,7 @@ class PictureWindow(Canvas):
             except:
                 print(f'{button_type} button is disabled')
         
-    def _button_enable(self, text, func, button_type='origin'):
+    def _button_enable(self, text, func, button_type):
         rely = 0.4 if button_type=='mask' else 0.5
         print(f'enable {button_type} button')
         self.type_to_button[button_type] = Button(self, text=text, command=func)
@@ -149,15 +154,16 @@ class PictureWindow(Canvas):
         self.img_switcher()
         
     def all_function_trigger(self):
-        self.create_buttons()
+        self.buttons_init()
 
-    def create_buttons(self):
+    def buttons_init(self):
         Button(self, text=" > ", command=self.next_image).place(relx=0.8, rely=0.5, anchor='center')
         Button(self, text=" < ", command=self.previous_image).place(relx=0.2, rely=0.5, anchor='center')
         Button(self, text=" Detect", command=self.img_predict).place(relx=0.5, rely=0.8, anchor='center')
         self.center_button = None
         self.lower_center_button = None
-        self.type_to_button = {'origin':self.center_button, 'mask':self.lower_center_button}
+        self.type_to_button = {'origin':self.center_button, 
+        'mask':self.lower_center_button}
         
     def img_predict(self):
         raw_img = self.cache[self.loc][0]
